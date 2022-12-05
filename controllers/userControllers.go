84fc8500407eeddac30b75620081e564dda61cc4
SethukumarJ <sethukumarj.76@gmail.com>
2022-12-05 09:57:50 +0530

    login function added, which include user validation and token creation, token is injected inside the cookie

--- controllers/userControllers.go
+++ controllers/userControllers.go
@@ -4,8 +4,11 @@ import (
 	"jwt/initializers"
 	"jwt/models"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -14,11 +17,11 @@ func Signup(c *gin.Context) {
 	var user models.User
 	//GEt email and password from request body
 	var body struct {
-		Email string
+		Email    string
 		Password string
 	}
 
-	if c.BindJSON(&user) != nil {
+	if c.Bind(&body) != nil {
 		c.JSON(400, gin.H{
 			"message": "invalid json",
 		})
@@ -32,14 +35,13 @@ func Signup(c *gin.Context) {
 		})
 		return
 	}
-	
 
 	//Save the user in the database
-	user = models.User{Email:body.Email,Password: string(hashedPassword)}
+	user = models.User{Email: body.Email, Password: string(hashedPassword)}
 	result := initializers.DB.Create(&user)
 
 	if result.Error != nil {
-		c.JSON(http.StatusBadRequest,gin.H{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "failed to create user",
 		})
 	}
@@ -47,3 +49,63 @@ func Signup(c *gin.Context) {
 	//respond
 	c.JSON(http.StatusOK, gin.H{})
 }
+
+func Login(c *gin.Context) {
+
+	//GEt email and password from request body
+	var body struct {
+		Email    string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(400, gin.H{
+			"message": "invalid json",
+		})
+		return
+	}
+
+	//Check if the user exists in the database
+	var user models.User
+
+	initializers.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "invalid credentials",
+		}) }
+
+		//Check if the password is correct
+
+		err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"message": "invalid credentials",
+			})
+			return
+		}
+
+		//Generate a jwt token
+		// Create a new token object, specifying signing method and the claims
+		// you would like it to contain.
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"sub": user.ID,
+			"exp": time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+		// Sign and get the complete encoded token as a string using the secret
+		tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))	
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "error signing token",
+			})
+			return
+		}
+
+		//respond
+		c.SetSameSite(http.SameSiteLaxMode)
+		c.SetCookie("token", tokenString, 3600, "/", "localhost", false, true)
+
+}
+
